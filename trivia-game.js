0f4f0d7c4c19feed553f96b079d6355fe85bb25c
Amanda Remo <amandaremo@ip-192-168-86-47.ec2.internal>
2022-07-31 10:00:30 -0400

    added list of questions and answer choices with the right answer in second html

---
+++ trivia-game.js
@@ -0,0 +1,107 @@
+// questions sources: 
+    // https://www.triviawell.com/questions/stranger-things
+    // https://netflixlife.com/2022/07/20/stranger-things-season-4-quiz-trivia/
+    // https://www.buzzfeed.com/jamiejirak1/quiz-stranger-things-2
+
+let listOfQuestions = [
+    {
+        question: "Where did the boys discover Eleven?",
+
+        answer1: "In the hospital.",
+        answer2: "In a diner.",
+        answer3: "In the woods.",
+        answer4: "In an arcade.",
+
+        rightAnswer: 3
+    },
+    {
+        question: "Who is Vecna?",
+
+        answer1: "001.",
+        answer2: "011.",
+        answer3: "010.",
+        answer4: "111.",
+
+        rightAnswer: 1
+    },
+    {
+        question: "Which actress did Dustin say his girlfriend is hotter than?",
+
+        answer1: "Phoebe Fox.",
+        answer2: "Phoebe Robinson.",
+        answer3: "Phoebe Tonkin.",
+        answer4: "Phoebe Cates.",
+
+        rightAnswer: 4
+    },
+    {
+        question: "What California town did the Byers family and Eleven move to?",
+
+        answer1: "Lenora Hills.",
+        answer2: "Los Angeles.",
+        answer3: "Virgin River.",
+        answer4: "Sacramento.",
+
+        rightAnswer: 1
+    },
+    {
+        question: "Which city does the show mainly take place in?",
+
+        answer1: "Phoenix.",
+        answer2: "Hawkins.",
+        answer3: "Lenora Hills",
+        answer4: "Virgin River",
+
+        rightAnswer: 2
+    },
+    {
+        question: "In Season 3, what does Alexei demand he get in order for him to talk?",
+
+        answer1: "Watermelon Slurpee.",
+        answer2: "Grape Slurpee.",
+        answer3: "Strawberry Slurpee.",
+        answer4: "Cherry Slurpee.",
+
+        rightAnswer: 4
+    },
+    {
+        question: "In Season 3, how many languages does Robin claim to speak?",
+
+        answer1: "1.",
+        answer2: "2.",
+        answer3: "3.",
+        answer4: "4.",
+
+        rightAnswer: 4
+    },
+    {
+        question: "Who doesn't die in season 4?",
+
+        answer1: "Dr. Brenner.",
+        answer2: "Alexei.",
+        answer3: "Chrissy Cunningham.",
+        answer4: "Eddie Munson.",
+
+        rightAnswer: 2
+    },
+    {
+        question: "What is the name of the shadow monster?",
+
+        answer1: "The Mind Flayer.",
+        answer2: "Demogorgon.",
+        answer3: "Vecna.",
+        answer4: "The Viner.",
+
+        rightAnswer: 1
+    },
+    {
+        question: "What is the name of Yuri's helicopter?",
+
+        answer1: "Anya.",
+        answer2: "Kamchatka.",
+        answer3: "Katinka.",
+        answer4: "Ekaterina.",
+
+        rightAnswer: 3
+    },
+]
